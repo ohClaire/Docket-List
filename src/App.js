e0413172b9453b0b3ec824755886cba23312ee00
Hannah.Celemen <hcelemen@icloud.com>
2024-02-10 15:37:09 -0700

    Add styling for container and cards

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { useState } from 'react';
+
 function App() {
+  const [cards, setCards] = useState([]);
 
   const callBackend = async () => {
     try {
@@ -16,27 +19,28 @@ function App() {
     } catch (error) {
       console.error('Error:', error);
     }
-  }
+  };
+
+  const addTaskCard = (card) => {
+    setCards((prevCards) => [...prevCards, card]);
+  };
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <button onClick={callBackend}>
-          Call Backend
-        </button>
+      <header>
+        <div className="title">Docker List</div>
+        <button onClick={callBackend}>Call Backend</button>
       </header>
+      <br></br>
+      <section>
+        <input placeholder="Enter task here" />
+        <button onClick={addTaskCard}>Add</button>
+      </section>
+      <section className="container">
+        <div className="card">Card 1</div>
+        <div className="card">Card 2</div>
+        <div className="card">Card 3</div>
+      </section>
     </div>
   );
 }
